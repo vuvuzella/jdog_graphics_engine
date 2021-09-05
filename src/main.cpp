ae237679f26365c81181e99cce63d5a13cc9288a
vuvuzella <j.tabacii@gmail.com>
2021-09-06 01:44:35 +1000

    fixed glad include before glfw3 made window render red background

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,11 @@
 #include<iostream>
+#include<glad/glad.h>   // ALWAYS include opengl loader before glfw!
 #include<GLFW/glfw3.h>
-#include<glad/glad.h>
+
+// TODO: What are these for?
+// #define GLFW_NO_GLU 1
+// #define GLFW_INCLUDE_GLCOREARB 1
+// #define GL_VERSION_3_0 1
 
 using namespace std;
 
@@ -28,15 +33,19 @@ int main() {
     glfwMakeContextCurrent(window);
 
     /* extension loader */
-    gladLoadGL();
+    if (!gladLoadGL()) {
+        cout << "Failed to initialize OpenGL context" << endl;
+    }
 
-    glClearColor( 0.4f, 0.3f, 0.4f, 0.0f);
+    // glClearColor( 0.4f, 0.3f, 0.4f, 0.0f);
+    static const GLfloat red[] = {1.0f, 0.0f, 0.0f, 1.0f };
 
     /* Loop until the user closes the window */
     while(!glfwWindowShouldClose(window)) {
 
         /* Render */
-        glClear(GL_COLOR_BUFFER_BIT);
+        // glClear(GL_COLOR_BUFFER_BIT);
+        glClearBufferfv(GL_COLOR, 0, red);  // render red background
 
         /* Swap the front and back buffer */
         glfwSwapBuffers(window);
